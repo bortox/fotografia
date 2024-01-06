e0ed8fce9f22db8f98c7f737f21aeb699ff6c8f9
Andrea Bortolotti <bort0x@tuta.io>
2024-01-06 13:18:48 +0100

    Quartz sync: Jan 6, 2024, 1:18 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -11,8 +11,9 @@ export default (() => {
     const url = new URL(`https://${cfg.baseUrl ?? "example.com"}`)
     const path = url.pathname as FullSlug
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
-
+    // Favicon da Twemoji
     const iconPath = joinSegments(baseDir, "static/icon.png")
+    const iconSvgPath = joinSegments(baseDir, "static/icon.svg")
     const ogImagePath = `https://${cfg.baseUrl}/static/og-image.png`
 
     return (
@@ -25,7 +26,8 @@ export default (() => {
         {cfg.baseUrl && <meta property="og:image" content={ogImagePath} />}
         <meta property="og:width" content="1200" />
         <meta property="og:height" content="675" />
-        <link rel="icon" href={iconPath} />
+        <link rel="icon" type="image/png" href={iconPath} />
+        <link rel="icon" type="image/svg+xml" href={iconSvgPath} />
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
